468fc3b8716ff8f30b311e3a15a0d5b90bc96751
RayneNoir <zhi88@live.nl>
2025-09-17 00:46:19 +0200

    Add files via upload

--- Code/grs_test.py
+++ Code/grs_test.py
@@ -3,13 +3,22 @@ import pandas as pd
 from scipy.stats import f, t
 
 def estimate_capm(marketReturns, assetReturns, riskfreeRate = 0.0025):
+    """
+    This function estimates parameters in the econometric CAPM model and their standard deviation.
+    It also tests the alphas with the t-statistic, F-statistic and p-values.
+
+    :param marketReturns: The excess market returns
+    :param assetReturns: The returns of the assets
+    :param riskfreeRate: the risk-free rate is set at 0.25%
+    :return: the CAPM parameters, covariance matrix, standard deviation of intercept, t-statistic, F-statistic, corresponding p-values and the market Sharpe ratio.
+    """
+
     assetReturns = assetReturns - riskfreeRate
     T = assetReturns.shape[0]
     n = assetReturns.shape[1]
-    assetVariance = assetReturns.var()
 
+    # Parameter estimation
     iota = np.ones_like(marketReturns)
-
     X = np.column_stack([iota, marketReturns])
     Q = np.linalg.inv(X.T @ X)
     parameters = Q @ X.T @ assetReturns
@@ -30,9 +39,6 @@ def estimate_capm(marketReturns, assetReturns, riskfreeRate = 0.0025):
     varianceResidual = np.diagonal(covarianceMatrix)
     standardDeviationAlpha = np.sqrt(Q[0,0]*varianceResidual)
 
-    ariv = np.mean(varianceResidual / assetVariance)
-    rms = np.sqrt(np.mean(alphas**2))
-
     # F-test
     testPortfolio = ((T - n - 1) / (n * (T - 2) * Q[0,0])) * (alphas.T @ np.linalg.inv(covarianceMatrix) @ alphas)
     pValuePortfolio = 1 - f.cdf(testPortfolio, n, T - n - 1)
@@ -41,4 +47,58 @@ def estimate_capm(marketReturns, assetReturns, riskfreeRate = 0.0025):
     testIndividual = alphas / standardDeviationAlpha
     pValueIndividual = 1 - t.cdf(np.abs(testIndividual), df=T-2)
 
-    return alphas, betas, covarianceMatrix, standardDeviationAlpha, ariv, rms, testPortfolio, pValuePortfolio, testIndividual, pValueIndividual
+    # Market Sharpe ratio
+    marketVariance = marketReturns.var()
+    marketMean = marketReturns.mean()
+    marketSharpeRatio = marketMean / np.sqrt(marketVariance)
+    return alphas, betas, covarianceMatrix, standardDeviationAlpha, testPortfolio, pValuePortfolio, testIndividual, pValueIndividual, marketSharpeRatio
+
+def estimate_factor_model(factorReturns, assetReturns, riskfreeRate = 0.0025):
+    assetReturns = assetReturns - riskfreeRate
+    T = assetReturns.shape[0]
+    n = assetReturns.shape[1]
+    k = factorReturns.shape[1]
+
+    # Parameter estimation
+    iota = np.ones_like(assetReturns, shape=(T,1))
+    F = np.column_stack([iota, factorReturns])
+    Q = np.linalg.inv(F.T @ F)
+    parameters = Q @ F.T @ assetReturns
+    alphas = parameters.iloc[0]
+    betas = parameters.iloc[1:4].T
+
+    yHat = (F @ parameters).to_numpy()
+    yHat = pd.DataFrame(
+        yHat,
+        index=assetReturns.index,
+        columns=assetReturns.columns
+    )
+
+    errors = assetReturns - yHat
+
+    covarianceMatrix = (errors.T @ errors) / (T - k - 1)
+    varianceResidual = np.diagonal(covarianceMatrix)
+    standardDeviationAlpha = np.sqrt(Q[0, 0] * varianceResidual)
+
+    # t-test
+    testIndividual = alphas / standardDeviationAlpha
+    pValueIndividual = 1 - t.cdf(np.abs(testIndividual), df=T - k - 1)
+
+    # F-test
+    populationVariance = ((T - k - 1)/T) * covarianceMatrix
+    populationFactorVariance = factorReturns.cov(ddof=0)
+    meanFactors = factorReturns.mean(axis=0).T
+
+    testFactor = (((T - n - k)/ (n * (1 + meanFactors.T @ np.linalg.inv(populationFactorVariance) @ meanFactors)))
+                  * (alphas.T @ np.linalg.inv(populationVariance) @ alphas))
+    pValueFactor = 1 - f.cdf(testFactor, n, T - n - k)
+
+    # Factor Sharpe ratio
+    factorSharpeRatio = np.sqrt(meanFactors.T @ np.linalg.inv(populationFactorVariance) @ meanFactors)
+
+    # Pricing error indicators
+    assetVariance = assetReturns.var()
+    rmsAlpha = np.sqrt(np.mean(alphas**2))
+    arivFactor = np.mean(varianceResidual / assetVariance)
+
+    return  alphas, betas, covarianceMatrix, standardDeviationAlpha, testFactor, pValueFactor, testIndividual, pValueIndividual, factorSharpeRatio, rmsAlpha, arivFactor
--- Code/question_two.py
+++ Code/question_two.py
@@ -8,13 +8,34 @@ factors = ut.open_file('F-F_Research_Data_5_Factors_2x3.csv', percentage=True)
 factors.drop(columns=['HML','RMW','RF'], inplace=True)
 
 # CAPM and test assets
-alphas, betas, sigmaMatrix, stdAlphas, ariv, rms, testPortfolio, pValuePortfolio, testIndividual, pValueIndividual = grs.estimate_capm(factors['Mkt-RF'], returns)
+alphas, betas, sigmaMatrix, stdAlphas, testPortfolio, pValuePortfolio, testIndividual, pValueIndividual, marketSharpeRatio = grs.estimate_capm(factors['Mkt-RF'], returns)
 result = pd.DataFrame({
     'Alpha [%]': 100*alphas.values.flatten(),
     'Std [%]': stdAlphas*100,
     't-statistic': testIndividual,
     'p-value': pValueIndividual,
 }, index=alphas.index).round({'Alpha [%]': 2, 'Std [%]': 2, 't-statistic': 3, 'p-value': 3})
+
+# 3 factor model
+(alphasFactor, betasFactor, sigmaFMatrix, stdAlphaFactor, testFactors, pValueFactors, testIndividualFactor, pValueIndividualFactor,
+ factorSharpeRatio, rmsAlphaFactor, arivFactor) = grs.estimate_factor_model(factors, returns, riskfreeRate = 0.0025)
+resultFactor = pd.DataFrame({
+    'Alpha [%]': 100*alphasFactor.values.flatten(),
+    'Std [%]': stdAlphaFactor*100,
+    't-statistic': testIndividualFactor,
+    'p-value': pValueIndividualFactor,
+}, index=alphasFactor.index).round({'Alpha [%]': 2, 'Std [%]': 2, 't-statistic': 3, 'p-value': 3})
+
+# The results
+print(50*"-")
 print('CAPM results')
+print(50*"-")
 print(result)
-print(f"CAPM: F-statistic: {testPortfolio:.3f}, p-value: {pValuePortfolio:.3f}")
+print(f"CAPM: F-statistic: {testPortfolio:.3f}, p-value: {pValuePortfolio:.3f} \nMarket Sharpe ratio: {marketSharpeRatio:.2f}")
+print(50*"-")
+print('3 Factor model')
+print(50*"-")
+print(resultFactor)
+print(f"Factor model: F-statistic: {testFactors:.3f}, p-value: {pValueFactors:.3f} \nFactor model Sharpe ratio: {factorSharpeRatio:.2f}"
+      f"\nAverage relative idiosyncratic variance (ARIV): {100*arivFactor:.2f}%, RMS pricing error: {rmsAlphaFactor*100:.2f}%")
+print(50*"-")
